41275a9ab2f3c0031f5e1bdd883044fe991565af
jessicadippolito <jessdipp@gmail.com>
2025-04-02 16:35:17 -0400

    in progress

--- dragon.py
+++ dragon.py
@@ -3,6 +3,7 @@ from cow import Cow
 class Dragon(Cow):
     def __init__(self, name):
         super().__init__(name)
+        self. image = None
 
     def can_breath_fire(self):
         return True
--- ice_dragon.py
+++ ice_dragon.py
@@ -3,6 +3,7 @@ from dragon import Dragon
 class IceDragon(Dragon):
     def __init__(self, name):
         super().__init__(name)
+        self.image = None
 
     def can_breathe_fire(self):
         return False
