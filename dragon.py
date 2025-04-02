db2f5efb672bf7275d8af391616ad03410040fdb
jessicadippolito <jessdipp@gmail.com>
2025-04-02 16:56:36 -0400

    complete

--- dragon.py
+++ dragon.py
@@ -3,7 +3,7 @@ from cow import Cow
 class Dragon(Cow):
     def __init__(self, name, image):
         Cow.__init__(self,name)
-        self. image = None
+        self. image = image
 
     def can_breath_fire(self):
         return True
--- ice_dragon.py
+++ ice_dragon.py
@@ -1,9 +1,10 @@
+from cow import Cow
 from dragon import Dragon
 
 class IceDragon(Dragon):
-    def __init__(self, name):
+    def __init__(self, name, image):
         Cow.__init__(self, name)
-        self.image = None
+        self.image = image
 
     def can_breathe_fire(self):
         return False
